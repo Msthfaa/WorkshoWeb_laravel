15a04e544736260d8b496307df79609c8e4d7f33
Msthfaa <10musthofaagungdistyawanrpl@gmail.com>
2025-09-23 18:43:59 +0700

    CRUD tabel employee

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $employees = Employee::latest()->paginate(5);
+        return view('pages.employee.index', compact('employees'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.employee.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+        'nama_lengkap' => 'required|string|max:255',
+        'email' => 'required|email|max:255',
+        'nomor_telepon' => 'required|string|max:20',
+        'tanggal_lahir' => 'required|date',
+        'alamat' => 'required|string|max:255',
+        'tanggal_masuk' => 'required|date',
+        'status' => 'required|string|max:50',
+        ]);
+        Employee::create($request->all());
+        return redirect()->route('pages.employee.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\EmployeeController;
+Route::resource('employees',EmployeeController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,5 +19,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
